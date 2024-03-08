b8509563822bfa9e67855b428cdf3704a796207f
Nikita Karpuk <bitxon.dev@gmail.com>
2024-03-08 09:23:47 +0100

    Add test for getById

--- test/integration/api-accounts.test.ts
+++ test/integration/api-accounts.test.ts
@@ -1,6 +1,6 @@
 import supertest from "supertest";
 import app from "../../src/app";
-import { RawAccount } from "../../src/api/account";
+import { Account, RawAccount } from "../../src/api/account";
 
 describe("API: Accounts", () => {
   test("GET /accounts", async () => {
@@ -24,6 +24,19 @@ describe("API: Accounts", () => {
         expect(response.body.email).toEqual(accountIn.email);
         expect(response.body.firstName).toEqual(accountIn.firstName);
         expect(response.body.lastName).toEqual(accountIn.lastName);
+      });
+  });
+
+  test("GET /accounts/:id", async () => {
+    const rawAccount: RawAccount = { email: "john1@mail.com", firstName: "John", lastName: "Doe" };
+    const account: Account = (await supertest(app).post("/accounts").send(rawAccount)).body;
+
+    return supertest(app)
+      .get(`/accounts/${account.id}`)
+      .send()
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(account);
       });
   });
 });
